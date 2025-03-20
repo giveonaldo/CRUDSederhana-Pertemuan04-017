b4f16bdc16c9c4e8b8b3697a1880e03012834fd1
giveonaldo <giveonaldo@gmail.com>
2025-03-20 12:54:22 +0700

    menambah label.

--- CRUDSederhana/Form1.Designer.cs
+++ CRUDSederhana/Form1.Designer.cs
@@ -28,13 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.NIM = new System.Windows.Forms.Label();
+            this.Nama = new System.Windows.Forms.Label();
+            this.Email = new System.Windows.Forms.Label();
+            this.Telepon = new System.Windows.Forms.Label();
+            this.Alamat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NIM
+            // 
+            this.NIM.AutoSize = true;
+            this.NIM.Location = new System.Drawing.Point(76, 70);
+            this.NIM.Name = "NIM";
+            this.NIM.Size = new System.Drawing.Size(27, 13);
+            this.NIM.TabIndex = 0;
+            this.NIM.Text = "NIM";
+            // 
+            // Nama
+            // 
+            this.Nama.AutoSize = true;
+            this.Nama.Location = new System.Drawing.Point(76, 106);
+            this.Nama.Name = "Nama";
+            this.Nama.Size = new System.Drawing.Size(35, 13);
+            this.Nama.TabIndex = 1;
+            this.Nama.Text = "Nama";
+            // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.Location = new System.Drawing.Point(76, 152);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(32, 13);
+            this.Email.TabIndex = 2;
+            this.Email.Text = "Email";
+            // 
+            // Telepon
+            // 
+            this.Telepon.AutoSize = true;
+            this.Telepon.Location = new System.Drawing.Point(76, 199);
+            this.Telepon.Name = "Telepon";
+            this.Telepon.Size = new System.Drawing.Size(46, 13);
+            this.Telepon.TabIndex = 3;
+            this.Telepon.Text = "Telepon";
+            // 
+            // Alamat
+            // 
+            this.Alamat.AutoSize = true;
+            this.Alamat.Location = new System.Drawing.Point(76, 237);
+            this.Alamat.Name = "Alamat";
+            this.Alamat.Size = new System.Drawing.Size(39, 13);
+            this.Alamat.TabIndex = 4;
+            this.Alamat.Text = "Alamat";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Alamat);
+            this.Controls.Add(this.Telepon);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.Nama);
+            this.Controls.Add(this.NIM);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label NIM;
+        private System.Windows.Forms.Label Nama;
+        private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.Label Telepon;
+        private System.Windows.Forms.Label Alamat;
     }
 }
 
