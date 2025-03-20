d95ce0c0f498a8303a82922b0df6032df7e0f2f4
giveonaldo <giveonaldo@gmail.com>
2025-03-20 12:57:19 +0700

    Menambah TextField.

--- CRUDSederhana/Form1.Designer.cs
+++ CRUDSederhana/Form1.Designer.cs
@@ -33,12 +33,17 @@
             this.Email = new System.Windows.Forms.Label();
             this.Telepon = new System.Windows.Forms.Label();
             this.Alamat = new System.Windows.Forms.Label();
+            this.txtNIM = new System.Windows.Forms.TextBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtTelepon = new System.Windows.Forms.TextBox();
+            this.txtAlamat = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // NIM
             // 
             this.NIM.AutoSize = true;
-            this.NIM.Location = new System.Drawing.Point(76, 70);
+            this.NIM.Location = new System.Drawing.Point(77, 32);
             this.NIM.Name = "NIM";
             this.NIM.Size = new System.Drawing.Size(27, 13);
             this.NIM.TabIndex = 0;
@@ -47,7 +52,7 @@
             // Nama
             // 
             this.Nama.AutoSize = true;
-            this.Nama.Location = new System.Drawing.Point(76, 106);
+            this.Nama.Location = new System.Drawing.Point(77, 68);
             this.Nama.Name = "Nama";
             this.Nama.Size = new System.Drawing.Size(35, 13);
             this.Nama.TabIndex = 1;
@@ -56,7 +61,7 @@
             // Email
             // 
             this.Email.AutoSize = true;
-            this.Email.Location = new System.Drawing.Point(76, 152);
+            this.Email.Location = new System.Drawing.Point(77, 114);
             this.Email.Name = "Email";
             this.Email.Size = new System.Drawing.Size(32, 13);
             this.Email.TabIndex = 2;
@@ -65,7 +70,7 @@
             // Telepon
             // 
             this.Telepon.AutoSize = true;
-            this.Telepon.Location = new System.Drawing.Point(76, 199);
+            this.Telepon.Location = new System.Drawing.Point(77, 161);
             this.Telepon.Name = "Telepon";
             this.Telepon.Size = new System.Drawing.Size(46, 13);
             this.Telepon.TabIndex = 3;
@@ -74,17 +79,57 @@
             // Alamat
             // 
             this.Alamat.AutoSize = true;
-            this.Alamat.Location = new System.Drawing.Point(76, 237);
+            this.Alamat.Location = new System.Drawing.Point(77, 199);
             this.Alamat.Name = "Alamat";
             this.Alamat.Size = new System.Drawing.Size(39, 13);
             this.Alamat.TabIndex = 4;
             this.Alamat.Text = "Alamat";
+            // 
+            // txtNIM
+            // 
+            this.txtNIM.Location = new System.Drawing.Point(157, 32);
+            this.txtNIM.Name = "txtNIM";
+            this.txtNIM.Size = new System.Drawing.Size(282, 20);
+            this.txtNIM.TabIndex = 5;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(157, 68);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(282, 20);
+            this.txtNama.TabIndex = 6;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(157, 114);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(282, 20);
+            this.txtEmail.TabIndex = 7;
+            // 
+            // txtTelepon
+            // 
+            this.txtTelepon.Location = new System.Drawing.Point(157, 161);
+            this.txtTelepon.Name = "txtTelepon";
+            this.txtTelepon.Size = new System.Drawing.Size(282, 20);
+            this.txtTelepon.TabIndex = 8;
+            // 
+            // txtAlamat
+            // 
+            this.txtAlamat.Location = new System.Drawing.Point(157, 199);
+            this.txtAlamat.Name = "txtAlamat";
+            this.txtAlamat.Size = new System.Drawing.Size(282, 20);
+            this.txtAlamat.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtAlamat);
+            this.Controls.Add(this.txtTelepon);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtNIM);
             this.Controls.Add(this.Alamat);
             this.Controls.Add(this.Telepon);
             this.Controls.Add(this.Email);
@@ -104,6 +149,11 @@
         private System.Windows.Forms.Label Email;
         private System.Windows.Forms.Label Telepon;
         private System.Windows.Forms.Label Alamat;
+        private System.Windows.Forms.TextBox txtNIM;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtTelepon;
+        private System.Windows.Forms.TextBox txtAlamat;
     }
 }
 
